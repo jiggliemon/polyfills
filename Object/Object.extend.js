f861a87a94809b7c238d00a0e42642fe2fe4523b
Chase <chase@agroism.com>
2011-07-05 10:39:47 -0700

    Fixing white space.

--- Object/Object.extend.js
+++ Object/Object.extend.js
@@ -1,9 +1,13 @@
 if(!Object.extend)
 Object.extend = function(original){
-	for (var i = 1, l = arguments.length; i < l; i++){
-		var extended = arguments[i] || {};
+	var  i =  1,
+	    ,l = arguments.length
+	    ,key
+	    ,extend
+	for (; i < l; i++){
+		extended = arguments[i] || {};
 
-		for (var key in extended){
+		for (key in extended){
 			original[key] = extended[key];
 		}
 	}
