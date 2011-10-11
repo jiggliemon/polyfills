95808783446bd4f94e790ee77a394d40c6ce2368
Chase <chase@agroism.com>
2011-10-11 10:03:16 -0700

    Making this a lot simpler.  The initial version was just stolen off the MDN site.

--- Array/Array.isArray.js
+++ Array/Array.isArray.js
@@ -6,15 +6,8 @@ for: [IE6,IE7,IE8,FF3,FF3.5,FF3.6,SF3.2,SF4,OP10.1]
 ...
 */
 define(function(){
-	var  builtInToString = Object.prototype.toString
-			,builtInToCall = Function.prototype.call
-			,callWithArgs = builtInToCall.bind(builtInToCall)
-			,argToString = function(o){
-				return callWithArgs(builtInToString, o);
-			};
-			
 	if(!Array.isArray)
-	Array.isArray = function(o) { 
-		return argToString(o) === '[object Array]';
+	Array.isArray = function (arg) {
+		return Object.prototype.toString.call(arg) == '[object Array]';
 	};
 })
