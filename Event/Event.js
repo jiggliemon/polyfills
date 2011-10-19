0489b2dbce0b40d59b0d534bcff10ee08db845b0
Chase Wilson <chase@agroism.com>
2011-10-18 20:22:05 -0700

    Binding the arguments of a fired latched event to any subsequent attempt to add a function to the latched event.

--- Event/Event.js
+++ Event/Event.js
@@ -1,65 +1,66 @@
 (function(global,_NAMESPACE_){
 
 var Events = _NAMESPACE_.Events = function(){
-	var  _events = {}
-		,_latched = {}
-		,REGEX = /:(latch(ed$)?)/i
-		
-	function removeLatched(type){
-		if(type.indexOf(':'))
-		if(REGEX.test(type)){
-			type = type.replace(REGEX,'');
-			console.log(type)
-			_latched[type] = true;
-		}
-		return type;
-	}
-	
-	return {
-		 addEvent: function(/* Sting */ type, /* Function */ callback){
-			var  type = removeLatched(type)
-				,events = _events[type] = _events[type] || [];
-			
-			if(!(typeof callback == 'function')) 
-				throw new TypeError(_NAMESPACE_.Events.errors.callback_must_be_function); 
+  var  _events = {}
+      ,_latched = {}
+      ,_arguments = {}
+      ,REGEX = /:(latch(ed$)?)/i
+    
+  function removeLatched(type){
+    if(type.indexOf(':'))
+    if(REGEX.test(type)){
+      type = type.replace(REGEX,'');
+      _latched[type] = 1;
+    }
+    return type;
+  }
   
-			(_latched[type])? setTimeout(callback,15) : events.push(callback);
-			return this;
-		}
+  return {
+     addEvent: function(/* Sting */ type, /* Function */ callback){
+      var  type = removeLatched(type)
+        ,events = _events[type] = _events[type] || [];
+      
+      if(!(typeof callback == 'function')) 
+        throw new TypeError(_NAMESPACE_.Events.errors.callback_must_be_function); 
+  
+      (_latched[type])? callback.apply(this,_arguments[type]) : events.push(callback);
+      return this;
+    }
 
-		,addEvents: function(/* Object */ events){
-			for(var key in events){
-				if(events.hasOwnProperty(key)){
-					this.addEvent(key,events[key]);
-				}
-			}
-			return this;
-		}
-		
-		,fireEvent: function(/* String */ type) {
-			
-			var  type = removeLatched(type)
-				,events = _events[type]
-				,length = events.length >>> 0
-				,args = Array.prototype.slice.call(arguments,1)
-				,i = 0
-	 
-			if(events && length) {
-				for (; i < length; i++) {
-					if (i in events) {
-						try{
-						events[i].apply(this,args);
-						} catch (e) { 
-							setTimeout(function(){
-								throw new Error(e);
-							},15);
-						}
-					}
-				}
-			}
-			return this;
-		}
-	};
+    ,addEvents: function(/* Object */ events){
+      for(var key in events){
+        if(events.hasOwnProperty(key)){
+          this.addEvent(key,events[key]);
+        }
+      }
+      return this;
+    }
+    
+    ,fireEvent: function(/* String */ type) {
+      
+      var  type = removeLatched(type)
+          ,events = _events[type]
+          ,length = events.length >>> 0
+          ,args = Array.prototype.slice.call(arguments,1)
+          ,i = 0
+      
+      if(_latched[type])
+        _arguments[type] = args;
+        
+      if(events && length) {
+        for (; i < length; i++) {
+          if (i in events) {
+            try{
+              events[i].apply(this,args);
+            } catch (e) { 
+              throw new Error('Event Error::'+type+':: '+e);
+            }
+          }
+        }
+      }
+      return this;
+    }
+  };
 };
 
 Events.errors = {
