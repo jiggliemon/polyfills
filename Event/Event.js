babf1fe72023a3ab4a4c95a2067552994b4cd224
Chase Wilson <chase@agroism.com>
2011-10-18 21:28:51 -0700

    Semicolons and stuff.

--- Event/Event.js
+++ Event/Event.js
@@ -4,29 +4,32 @@ var Events = _NAMESPACE_.Events = function(){
   var  _events = {}
       ,_latched = {}
       ,_arguments = {}
-      ,REGEX = /:(latch(ed$)?)/i
+      ,REGEX = /:(latch(ed$)?)/i;
     
   function removeLatched(type){
-    if(type.indexOf(':'))
-    if(REGEX.test(type)){
-      type = type.replace(REGEX,'');
-      _latched[type] = 1;
+    if(type.indexOf(':')){
+      if(REGEX.test(type)){
+        type = type.replace(REGEX,'');
+        _latched[type] = 1
+      }
     }
     return type;
   }
   
   return {
-     getEvents: function(){
-       return Object.keys(_events);
+     getEvents: function(key){
+       var events =_events[key];
+       return (typeof key === 'string') ? events?events:[] :Object.keys(_events);
     }
     ,addEvent: function(/* Sting */ type, /* Function */ callback){
       var  type = removeLatched(type)
-        ,events = _events[type] = _events[type] || [];
+          ,events = _events[type] = _events[type] || [];
       
-      if(!(typeof callback == 'function')) 
+      if(!(typeof callback == 'function')) {
         throw new TypeError(_NAMESPACE_.Events.errors.callback_must_be_function); 
-  
-      (_latched[type])? callback.apply(this,_arguments[type]) : events.push(callback);
+      }
+      
+      (_latched[type]) ? callback.apply(this,_arguments[type]) : events.push(callback);
       return this;
     }
 
@@ -46,7 +49,7 @@ var Events = _NAMESPACE_.Events = function(){
           ,events = _events[type]
           ,length = events.length >>> 0
           ,args = Array.prototype.slice.call(arguments,1)
-          ,i = 0
+          ,i = 0;
       
       if(events && length) {
         for (; i < length; i++) {
