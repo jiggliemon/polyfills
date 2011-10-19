a417dec14b426baed311551f46d26b9738d0f05e
Chase Wilson <chase@agroism.com>
2011-10-19 15:20:07 -0700

    Bringint Array/Array.prototype.every.js up to spec.

--- Source/Array/Array.prototype.every.js
+++ Source/Array/Array.prototype.every.js
@@ -6,17 +6,20 @@ for: [IE6,IE7,IE8]
 ...
 */
 define(function(){
-	if (!Array.prototype.every)
-	Array.prototype.every = function(block /*, context */) {
-		var  context = arguments[1]
-			,i = 0
-			,length = this.length;
-			
-		for (; i < length; i++){
-			if (!block.call(context, this[i])){
-				return false;
-			}
-		}
-		return true;
-	};
+  if (!Array.prototype.every){
+    Array.prototype.every = function(block /*, context */) {
+      var  context = arguments[1]
+          ,i = 0
+          ,length = this.length;
+      
+      for (; i < length; i++){
+        if(i in this){
+          if (!block.call(context, this[i], i, this)){
+            return false;
+          }
+        }
+      }
+      return true;
+    };
+  }
 });
