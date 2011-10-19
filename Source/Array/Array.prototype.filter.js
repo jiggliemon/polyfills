ce66c3a91f264569f0cfdd0f17aab17eeb0b0d8b
Chase Wilson <chase@agroism.com>
2011-10-19 15:41:00 -0700

    Bringing this Array.prototype.filter.js fill up to ES5 spec.  It wasn't passing the index and the array in the previous version.

--- Source/Array/Array.prototype.filter.js
+++ Source/Array/Array.prototype.filter.js
@@ -6,16 +6,21 @@ for: [IE6,IE7,IE8]
 ...
 */
 define(function(){
-	if (!Array.prototype.filter)
-	Array.prototype.filter = function ( block /*, context */) {
-		var  values = []
-				,context = arguments[1]
-				,i = 0
-				,length = this.length;
-			
-		for (; i < length; i++)
-			if (block.call(context, this[i]))
-				values.push(this[i]);
-		return values;
-	};
+  if (!Array.prototype.filter){
+    Array.prototype.filter = function ( block /*, context */) {
+      var  values = []
+          ,context = arguments[1]
+          ,i = 0
+          ,length = this.length;
+      
+      for (; i < length; i++){
+        if(i in this){
+          if (block.call(context, this[i], i, this)){
+            values.push(this[i]);
+          }
+        }
+      }
+      return values;
+    };
+  }
 });
